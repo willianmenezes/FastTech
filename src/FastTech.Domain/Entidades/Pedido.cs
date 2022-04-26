1956e92f1ca6797bae15131fefaaaff574e5a317
Willian Menezes <willian_menezes_santos@hotmail.com>
2022-04-25 22:05:27 -0300

    Criado logicas para adicionar item no pedido e remover item do pedido.

--- src/FastTech.Domain/Entidades/Pedido.cs
+++ src/FastTech.Domain/Entidades/Pedido.cs
@@ -10,4 +10,48 @@ internal class Pedido : Entity
 
     private List<PedidoItem> _pedidoItems;
     public IReadOnlyCollection<PedidoItem> PedidoItens => _pedidoItems;
+
+    public Pedido()
+    {
+        _pedidoItems = new List<PedidoItem>();
+    }
+
+    public void CalcularValorTotal()
+    {
+        ValorTotal = _pedidoItems.Sum(pedidoItem => pedidoItem.CalcularValor());
+    }
+
+    public void AdicionarItemNoPedido(PedidoItem pedidoItem)
+    {
+        if (ExistePedidoItem(pedidoItem)
+            is var itemEncontrado && itemEncontrado != null)
+        {
+            itemEncontrado.AdicionarQuantidade(pedidoItem.Quantidade);
+            pedidoItem = itemEncontrado;
+        }
+        else
+        {
+            _pedidoItems.Add(pedidoItem);
+        }
+
+        pedidoItem.CalcularValor();
+        CalcularValorTotal();
+    }
+
+    public void RemoverItem(PedidoItem pedidoItem)
+    {
+        if (ExistePedidoItem(pedidoItem)
+           is var itemEncontrado && itemEncontrado == null)
+        {
+            throw new Exception("O item nao foi encontrado no pedido. Item invalido.");
+        }
+
+        _pedidoItems.Remove(itemEncontrado);
+        CalcularValorTotal();
+    }
+
+    private PedidoItem ExistePedidoItem(PedidoItem item)
+    {
+        return _pedidoItems.FirstOrDefault(pedidoItem => pedidoItem.ProdutoId == item.ProdutoId);
+    }
 }
--- src/FastTech.Domain/Entidades/PedidoItem.cs
+++ src/FastTech.Domain/Entidades/PedidoItem.cs
@@ -2,10 +2,21 @@
 
 internal class PedidoItem : Entity
 {
-    public Guid PedidoId { get; set; }
+    public Guid PedidoId { get; private set; }
+    public Guid ProdutoId { get; private set; }
     public int Quantidade { get; private set; }
     public decimal ValorUnitario { get; private set; }
     public string NomeProduto { get; private set; }
 
     public Pedido Pedido { get; set; }
+
+    public decimal CalcularValor()
+    {
+        return ValorUnitario * Quantidade;
+    }
+
+    public void AdicionarQuantidade(int quantidade)
+    {
+        Quantidade += quantidade;
+    }
 }
